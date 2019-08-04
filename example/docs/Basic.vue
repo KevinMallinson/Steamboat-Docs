dfd1da03235e8175da9a2216cbe7d54095e8a8db
Kevin Mallinson <kevin.mallinson@outlook.com>
2019-08-04 12:52:36 +1000

    Clean Up Example

---
+++ example/docs/Basic.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    <h1>Test</h1>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  components: {
+  },
+  name: 'DemoTest',
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
