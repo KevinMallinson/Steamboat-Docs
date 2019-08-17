97131cb89fb9db030bc17410489d291c56d7fbd7
Kevin Mallinson <kevin.mallinson@outlook.com>
2019-08-17 12:21:46 +1000

    Broken SideBar

--- boot/src/components/SideBar.vue
+++ boot/src/components/SideBar.vue
@@ -2,61 +2,63 @@
     import Vue from 'vue';
 
     export default {
-        render(createElement) {
+        functional: true,
+        render(createElement, context) {
+
             let searchBar = createElement('div', {
                 'class': 'search-box-container'
             }, [
                 createElement('b-input', {
                     props: {
-                        placeholder: 'Search'
+                        placeholder: 'Filter'
                     }
                 })
             ]);
 
             //Currently, we have no nav items, so we need to kick it off.
+            //The reference to this object is in the `navbar.children` variable
+            //Thus allowing us to build its children.
             let nav = createElement('ul', { 
-                attrs: {
-                    class: 'nav flex-column left-navbar' }
+                    'class': [ 'nav', 'flex-column', 'left-navbar' ]
                 }, []
             );
 
-            let selectedList = nav;
-
+            let navbar = createElement('nav', { 'class': [ 'col-md-2', 'd-none', 'd-md-block', 'bg-light', 'sidebar' ] }, [ searchBar, nav ]);
+            
             //Depth First Search.
             let stack = [ ...Vue.prototype.NAV_ITEMS, null ];
 
-            let depth = 0;
             while(stack.length > 0) {
                 let currentNode = stack.shift();
 
                 if (currentNode == null) {
-                    selectedList = selectedList.parent;
+                    nav = nav.parent;
                     continue;
                 }
-            
-                // let link = createElement('a', { attrs: { class: 'nav-link', href: currentNode.link } }, [ currentNode.text ]);            
-                let link = createElement('router-link', { 'class': 'nav-link', props: { to: currentNode.link }, } , [ currentNode.text ]);
-                let selectedItem = createElement('li', { 'class': 'nav-item' }, [ link ]);
-                selectedItem.parent = selectedList;
-                selectedList.children.push(selectedItem);
+                    
+                let selectedItem = createElement('li', { 'class': ['nav-item'] }, [ 
+                    createElement('router-link', { 'class': ['nav-link'], props: { to: currentNode.link }, } , [ currentNode.text ]) 
+                ]);
 
-                //If we are pushing to the stack, then depth goes up
+                selectedItem.parent = nav;
+                nav.children.push(selectedItem);
+
                 //We use a single null to let us know when we need to reduce depth
                 if (currentNode.children && currentNode.children.length > 0) {
                     stack  = [...currentNode.children, null, ...stack];
-                    selectedList = createElement('ul', { attrs: { class: 'nav flex-column ml-3' } }, []);  
-                    selectedList.parent = selectedItem;
-                    selectedItem.children.push(selectedList);
+                    nav = createElement('ul', { 'class': [ 'nav', 'flex-column', 'ml-3' ] }, []);  
+                    nav.parent = selectedItem;
+                    selectedItem.children.push(nav);
                 }
             }
 
-            return createElement('nav', { attrs: { class: 'col-md-2 d-none d-md-block bg-light sidebar' } }, [ searchBar, nav ]);
+            return navbar;
         }
     }
 </script>
 
 
-<style lang="scss" scoped>
+<style lang="scss">
     $navHeight: 56px;
 
     .search-box-container {
@@ -70,11 +72,5 @@
         top: $navHeight;
         min-height: calc(100vh - #{$navHeight});
         border-right: 1px solid rgba(0, 0, 0, 0.05);
-    }
-
-    .left-navbar-item {
-    }
-
-    .left-navbar-item a{
     }
 </style>
